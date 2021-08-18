8cacdf4d0f308fb13bea11f1bced6650c2fe33a6
Jesper Turesson <Jturesson90@gmail.com>
2021-08-18 14:52:04 +0200

    Added code

---
+++ DrolegamesUnityMobileTools/Assets/DrolegamesTools/IO/SaveDataBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Drolegames.IO
+{
+    using System;
+    using UnityEngine;
+
+    [Serializable]
+    public class SaveDataBase
+    {
+        public double totalPlayingTime;
+
+
+        public static SaveDataBase FromBytes(byte[] data) => data != null ? SaveDataBase.FromString(System.Text.ASCIIEncoding.Default.GetString(data)) : null;
+        public static SaveDataBase FromString(string s) => JsonUtility.FromJson<SaveDataBase>(s);
+        public byte[] ToBytes() => System.Text.ASCIIEncoding.Default.GetBytes(ToString());
+        public override string ToString() => JsonUtility.ToJson(this, false);
+    }
+}

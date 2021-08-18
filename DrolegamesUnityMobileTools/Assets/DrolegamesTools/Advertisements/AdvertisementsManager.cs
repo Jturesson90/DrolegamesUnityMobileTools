357f3f1b1df38a1a3f1eb93464bd480b06695c5a
Jesper Turesson <Jturesson90@gmail.com>
2021-08-18 16:56:06 +0200

    Upgraded UnityAds

--- DrolegamesUnityMobileTools/Assets/DrolegamesTools/Advertisements/AdvertisementsManager.cs
+++ DrolegamesUnityMobileTools/Assets/DrolegamesTools/Advertisements/AdvertisementsManager.cs
@@ -1,16 +1,14 @@
 ﻿namespace Drolegames.Advertisements
 {
     using System;
-    using Drolegames.Utils;
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.Advertisements;
-    using UnityEngine.Events;
-    using UnityEngine.SceneManagement;
+    using Drolegames.Utils;
 
-    public class AdvertisementsManager : MonoBehaviour, IUnityAdsListener
+    public class AdvertisementsManager : Singleton<AdvertisementsManager>, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
     {
+        public bool testMode = false;
+        public bool enablePerPlacementLoad = false;
         public string iOSGameId = string.Empty;
         public string androidGameId = string.Empty;
         public string mockGameId = string.Empty;
@@ -35,42 +33,65 @@
         public static event EventHandler<AdsFinishedEventArgs> OnRewardVideoSuccess;
 
         private string currentId;
-        private void OnEnable()
-        {
-            Advertisement.AddListener(this);
-        }
-        private void OnDisable()
-        {
-            Advertisement.RemoveListener(this);
-        }
         private void Start()
         {
 #if UNITY_IOS
             gameId = iOSGameId;
 #elif UNITY_ANDROID
             gameId = androidGameId;
-#else 
+#else
             gameId = mockGameId;
-#endif   
+#endif
+            Initialize();
         }
         public void Initialize()
         {
-            Advertisement.Initialize(gameId, false);
-        }
-        public void OnUnityAdsDidError(string message)
-        {
-            Debug.LogError("AdManager OnUnityAdsDidError " + message);
+            Advertisement.Initialize(gameId, testMode, enablePerPlacementLoad, this);
         }
 
-        public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+        public bool GetIsReady() => Advertisement.IsReady(rewardVideoID);
+        public bool ShowRewardVideo(string id)
         {
-            if (rewardVideoID.Equals(placementId))
+            if (!GetIsReady()) return false;
+            Advertisement.Show(rewardVideoID, this);
+            currentId = id;
+            return true;
+        }
+
+        #region IUnityAdsInitializationListener
+        public void OnInitializationComplete()
+        {
+            Debug.Log("Unity Ads initialization complete.");
+            Advertisement.Load(gameId, this);
+        }
+        public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+        {
+            Debug.LogWarning($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+        }
+        #endregion
+
+        #region IUnityAdsLoadListener
+        public void OnUnityAdsAdLoaded(string placementId)
+        {
+            if (placementId.Equals(rewardVideoID))
             {
-                OnRewardVideoSuccess?.Invoke(this, new AdsFinishedEventArgs(showResult, currentId));
+                AdsIsReady = true;
             }
         }
 
-        public void OnUnityAdsDidStart(string placementId)
+        public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+        {
+            Debug.LogWarning($"Unity Ads Load Failed: {error.ToString()} - {message}");
+        }
+        #endregion
+
+        #region IUnityAdsShowListener
+        public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+        {
+            Debug.LogWarning($"Unity Ads Show Failed: {error.ToString()} - {message}");
+        }
+
+        public void OnUnityAdsShowStart(string placementId)
         {
             if (placementId.Equals(rewardVideoID))
             {
@@ -78,30 +99,41 @@
             }
         }
 
-        public void OnUnityAdsReady(string placementId)
+        public void OnUnityAdsShowClick(string placementId)
         {
             if (placementId.Equals(rewardVideoID))
             {
-                AdsIsReady = true;
+                AdsIsReady = false;
             }
         }
-        public bool GetIsReady() => Advertisement.IsReady(rewardVideoID);
-        public bool ShowRewardVideo(string id)
+
+        public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
         {
-            if (!GetIsReady()) return false;
-            Advertisement.Show(rewardVideoID);
-            currentId = id;
-            return true;
+            if (rewardVideoID.Equals(placementId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
+            {
+                OnRewardVideoSuccess?.Invoke(this, new AdsFinishedEventArgs((AdvertisementResult)showCompletionState, currentId));
+            }
+            Advertisement.Load(gameId, this);
         }
+        #endregion
+
+
+
     }
     public class AdsFinishedEventArgs : EventArgs
     {
-        public ShowResult showResult;
+        public AdvertisementResult showResult;
         public string id;
-        public AdsFinishedEventArgs(ShowResult showResult, string id)
+        public AdsFinishedEventArgs(AdvertisementResult showResult, string id)
         {
             this.showResult = showResult;
             this.id = id;
         }
+    }
+    public enum AdvertisementResult
+    {
+        SKIPPED = 0,
+        COMPLETED = 1,
+        UNKNOWN = 2
     }
 }
